bc6d65150048310c93ec3e98d4ac4221c2c8088a
lyj9186 <lyj9186@gmail.com>
2022-06-24 17:39:48 +0900

    master

--- app.js
+++ app.js
@@ -2,3 +2,25 @@
 // +, -, /, *
 //  add, minus, div, multi 
 
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    minus: function(a, b){
+        console.log(a - b);
+    },
+    div: function(a, b){
+        console.log(a / b);
+    },
+    multi: function(a, b){
+        console.log(a * b);
+    },    
+}
+
+calculator.add(8, 2);
+calculator.minus(8, 2);
+calculator.div(8, 2);
+calculator.multi(8, 2);
+
+
+
